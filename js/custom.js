62cf7d582f7a0b44e919fb8c799ef8f8894b1dba
Hamza ali <hamzabinsultan313@gmail.com>
2024-09-30 06:32:55 +0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+AOS.init({ once: true })
+$(document).ready(function () {
+  var url = window.location.pathname;
+  var filename = url.substring(url.lastIndexOf('/') + 1);
+  if (filename == "") {
+    filename = "index.php"
+  }
+  $("header .nav-item .nav-link").removeClass("active");
+  $(`header .nav-item .nav-link[href="${filename}"]`).addClass("active")
+});
+$(window).scroll(function (e) {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 100) {
+    $("header").addClass("scroll");
+  } else {
+    $("header").removeClass("scroll");
+  }
+});
+
